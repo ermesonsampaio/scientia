dd340a2ad33b8e44389c49c8745859c5b3733fa8
Ermeson Sampaio <ermeson.sampaio.queiroz@gmail.com>
2022-07-07 15:41:05 -0300

    revert(bottomsheet): delete BottomSheet component

--- mobile/src/components/index.ts
+++ mobile/src/components/index.ts
@@ -1,9 +1,5 @@
 export { default as PrimaryButton, PrimaryButtonProps } from './PrimaryButton';
 export { default as TextInput, TextInputProps } from './TextInput';
 export { default as SearchInput } from './SearchInput';
-export {
-  default as BottomSheet,
-  BottomSheetProps,
-  BottomSheetRef as BottomSheetRef,
-} from './BottomSheet';
 export { default as Alert, Action, AlertProps } from './Alert';
+export { default as Skeleton } from './Skeleton';
